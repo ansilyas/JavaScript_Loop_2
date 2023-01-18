3e0e803ff0ab160d7d8416059271dfd34150dee4
ANSILYAS <114913715+ansilyas@users.noreply.github.com>
2023-01-18 23:24:31 +0500

    Add files via upload

---
+++ While Loop/js/script.js
@@ -0,0 +1,21 @@
+// While Loop:
+let n=prompt("Enter the value of n")
+n=Number.parseInt(n)
+
+let i=0;
+while(i<n){
+    console.log(i)
+    i++;
+}
+
+// Do While Loop:
+/*
+ let n=prompt("Enter the value of n")
+n=Number.parseInt(n)
+
+let i=0;
+do{
+    console.log(i)
+    i++;
+}while (i<n)
+*/
